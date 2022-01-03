c7a7aa66e0626737091d81883eea47c1d18fb28c
aaron <aron.moles@gmail.com>
2022-01-03 15:29:39 +0100

    feat: add error

--- src/app/Server.ts
+++ src/app/Server.ts
@@ -36,6 +36,7 @@ export default class Server {
     // TODO Exportar interfaz Server y renombrar a Express Server
     private readonly logger: Logger;
 
+    const aaaa = 0;
 
     private readonly env: Env<EnvKey>;
 
