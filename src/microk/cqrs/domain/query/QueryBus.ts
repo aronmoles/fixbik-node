d9950d7d243d131a103b1c960cc017e19e2c533e
aaron <aron.moles@gmail.com>
2022-01-14 18:48:18 +0100

    fix: remove QueryResponse in QueryBus

--- src/microk/cqrs/domain/query/QueryBus.ts
+++ src/microk/cqrs/domain/query/QueryBus.ts
@@ -1,6 +1,5 @@
 import Query from './Query';
-import { QueryResponse } from './QueryResponse';
 
 export default interface QueryBus {
-    ask<R extends QueryResponse>(query: Query): Promise<R>;
+    ask<R>(query: Query): Promise<R>;
 }
